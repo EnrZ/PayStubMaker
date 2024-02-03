36a078f663fe7f2fd12dfdef972d307320246732
EnrZ <ez_25_25@yahoo.com>
2024-02-03 09:59:37 -0600

    first commit

---
+++ src/main/java/dev/ez/PayStubMaker/PayStubMakerApplication.java
@@ -0,0 +1,13 @@
+package dev.ez.PayStubMaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayStubMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayStubMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ez/PayStubMaker/PayStubMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ez.PayStubMaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayStubMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
